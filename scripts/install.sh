2dc5a153824791515906aeda61fd3dadc97ec291
Vahid Mohammadi <vahidm1467@gmail.com>
2022-04-12 19:43:14 +0430

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,7 @@ init() {
     curl $script_file -o "${destination}/commit-msg";
     chmod u+x "${destination}/commit-msg";
 
-    printf "\nInstalled \e[33mcommit-msg\033[0m hook in \e[32m%s\033[0m." "$destination";
+    printf "\nInstalled \e[33mcommit-msg\033[0m hook in \e[32m%s\033[0m.\n" "$destination";
     echo "More about Conventional Commits: https://www.conventionalcommits.org/";
 }
 
