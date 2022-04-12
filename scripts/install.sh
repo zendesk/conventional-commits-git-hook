862526fdee74ab218d81fefab5034278acd17dcd
Vahid Mohammadi <vahidm1467@gmail.com>
2022-04-12 19:37:07 +0430

    Remove warnings and resolve issues based on shellcheck.net

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,18 +2,18 @@
 
 script_file="https://raw.githubusercontent.com/tapsellorg/conventional-commits-git-hook/master/commit-msg-hook.sh"
 
-function init {
-    if [ ! -z $1 ]; then
+init() {
+    if [ -n "$1" ]; then
         echo "Unsupported argument '$1'"
     fi
 
     destination="${PWD}/.git/hooks"
 
-    download_status=$(curl $script_file -o "${destination}/commit-msg")
+    curl $script_file -o "${destination}/commit-msg"
     chmod u+x "${destination}/commit-msg"
 
-    echo -e "\nInstalled \e[33mcommit-msg\033[0m hook in \e[32m$destination\033[0m."
+    printf "\nInstalled \e[33mcommit-msg\033[0m hook in \e[32m%s\033[0m." "$destination"
     echo "More about Conventional Commits: https://www.conventionalcommits.org/"
 }
 
-init $1
+init "$1"
