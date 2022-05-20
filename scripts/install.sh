a60c7485bc96c81b12fc58f6674b63c21cd67850
holmeso <holmeso@users.noreply.github.com>
2022-05-20 14:16:48 +1000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-script_file="https://raw.githubusercontent.com/cytomine/conventional-commits-git-hook/master/commit-msg-hook.sh";
+script_file="https://raw.githubusercontent.com/holmeso/conventional-commits-git-hook/master/commit-msg-hook.sh";
 
 init() {
     if [ -n "$1" ]; then
@@ -12,7 +12,7 @@ init() {
     curl $script_file -o "${destination}/commit-msg";
     chmod u+x "${destination}/commit-msg";
 
-    printf "\nInstalled \e[33mcommit-msg\033[0m hook in \e[32m%s\033[0m.\n" "$destination";
+    printf "\nInstalled \x1B[33mcommit-msg\033[0m hook in \x1B[32m%s\033[0m.\n" "$destination";
     echo "More about Conventional Commits: https://www.conventionalcommits.org/";
 }
 
